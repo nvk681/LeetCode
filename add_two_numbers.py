a0ec935a4773a13d81cb9cd12f736c5274e040f4
Nuthalapati Venkata Krishna Chaitanya <nvk.681@gmail.com>
2022-02-24 17:08:36 -0500

    add: optimize ll sum

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,41 +1,29 @@
 # Definition for singly-linked list.
-from typing import Optional
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # if l1.val == 0:
-        #     return l2
-        # if l2.val == 0:
-        #     return l1
-        num1 = 0
-        num2 = 0
-        i = 1
-        while l1 is not None: 
-            num1 = num1+(l1.val*i)
-            i = i*10 
-            l1 = l1.next
-        i = 1
-        while l2 is not None: 
-            num2 = num2+(l2.val*i)
-            i = i*10 
-            l2 = l2.next
-        l1 = num1+num2
-        if l1 != 0:
-            temp = ListNode(l1%10)
-            l1 = l1//10
-            head = temp
-            while l1 != 0:
-                tem = l1%10
-                l1 = l1//10
-                a = ListNode(tem)
-                # a.next = temp
-                temp.next = a
-                temp = a
-        else: 
-            return ListNode()
-        return head
+    def addTwoNumbers(self, l1, l2):
+        carry = 0
+        return_value = None
+        temp = None
+        while l1 is not None or l2 is not None:
+            a = 0 if l1 is None else l1.val
+            b = 0 if l2 is None else l2.val
+            sum_value = a+b+carry
+            carry = int((sum_value)/10)
+            new_item = ListNode(sum_value%10)
+            if return_value is None:
+                return_value = new_item
+                temp = new_item
+            else:
+                temp.next = new_item
+                temp = temp.next
+            l1 = None if l1 is None else l1.next
+            l2 = None if l2 is None else l2.next
+        if carry > 0:
+            temp.next = ListNode(carry)
             
+        return return_value
+       
