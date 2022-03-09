fef996aad84e5289bc443faa46e47e26ad0ad45b
Nuthalapati Venkata Krishna Chaitanya <nvk.681@gmail.com>
2022-03-09 17:00:34 -0500

    add: original array from double array

--- find_original_array_from_doubled_array.py
+++ find_original_array_from_doubled_array.py
@@ -1,29 +1,33 @@
 class Solution:
     def findOriginalArray(self, changed):
-        if len(changed)%2 != 0:
-            return []
+        if len(changed)%2 != 0: return []
         changed.sort()
-        new_one = []
-        zero_count = 0
-        if changed.count(0)%2 == 1:
-            return []
-        else: 
-            while changed.count(0) > 0:
-                zero_count += 1
-                changed.remove(0) 
-        i = 0
-        half_len =  (len(changed)//2)
-        while i < half_len:
-            if (2*changed[i] not in changed):
-                return []
+        original = []
+        count = {}
+        
+        for i in changed:
+            if i in count:
+                count[i] += 1
             else:
-                new_one.append(changed[i])
-                changed.remove(changed[i]*2)
-                i += 1
-        if zero_count > 0:
-            new_one = new_one+([0]*(zero_count//2))
-        return new_one
-
+                count[i] = 1
+        for i in changed:
+            if count[i] <= 0: continue
+            count[i] -= 1
+            half = int(i/2)
+            
+            if i%2 == 0 and half in count and count[half] > 0:
+                original.append(half)
+                count[half] -= 1
+                continue
+                
+            double = i*2
+            
+            if double in count and count[double] > 0:
+                count[double] -= 1
+                original.append(i)
+                continue
+            return []
+        
+        return original
 s = Solution()
-print(s.findOriginalArray([0,0,0,0]))
-# print(s.findOriginalArray([0,1,0,0]))
+print(s.findOriginalArray([4,8,2,16,1,8]))
