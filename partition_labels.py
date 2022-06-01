8b912548be010deed3aea2c94b5d1aedab2b1357
Nuthalapati Venkata Krishna Chaitanya <nvk.681@gmail.com>
2022-06-01 00:30:18 -0400

    add: learnt and redid the code

--- partition_labels.py
+++ partition_labels.py
@@ -1,11 +1,12 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         last = {c: i for i, c in enumerate(s)}
-        j = anchor = 0
-        ans = []
+        last_point, start = 0, 0
+        result = []
         for i, c in enumerate(s):
-            j = max(j, last[c])
-            if i == j:
-                ans.append(i - anchor + 1)
-                anchor = i + 1
-        return ans
+            last_point = last_point if last_point > last[c] else last[c]
+            if last_point == i:
+                result.append(last_point - start + 1)
+                last_point += 1
+                start = last_point
+        return result
