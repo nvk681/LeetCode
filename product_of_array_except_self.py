44c5c4a1a8ba270e24e4f7830f03afb6fe231a29
Nuthalapati Venkata Krishna Chaitanya <nvk.681@gmail.com>
2022-06-01 23:05:05 -0400

    add: copied solution need to understand

---
+++ product_of_array_except_self.py
@@ -0,0 +1,10 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1]*len(nums)
+        for i in range(1, len(nums)):
+            ans[i] = ans[i-1]*nums[i-1]
+        rightProduct = 1
+        for i in range(len(nums)-1, -1, -1):
+            ans[i] = ans[i] * rightProduct
+            rightProduct *= nums[i]
+        return ans
