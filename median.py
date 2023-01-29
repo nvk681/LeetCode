83974c64d06a9075a1134c7d2a01bb00f4a5c270
Nuthalapati Venkata Krishna Chaitanya <nvk.681@gmail.com>
2023-01-28 22:23:09 -0500

    add: new solution

--- median.py
+++ median.py
@@ -1,46 +1,28 @@
 def findMedianSortedArrays(nums1, nums2) -> float:
-        index1,index2 = 0,0
-        total_lenght = len(nums1)+len(nums2)
-        median = list()
-        answer = list()
-        if total_lenght/2 == total_lenght//2:
-            median.append(total_lenght//2)
-            median.append(total_lenght//2+1)
-        else: 
-            median.append(total_lenght//2+1)
-        while len(answer) != len(median):
-            if (index1+index2+1) in median:
-                ans = None
-                if index1 >= len(nums1) and index2 < len(nums2):
-                    ans = nums2[index2]
-                    index2 += 1
-                if index2 >= len(nums2) and index1 < len(nums1):
-                    ans = nums1[index1]
-                    index1 += 1
-                if ans is None: 
-                    if nums1[index1] < nums2[index2]:
-                        ans = nums1[index1]
-                        index1 += 1
-                    else:
-                        ans = nums2[index2]
-                        index2 += 1
-                answer.append(ans)
-            else: 
-                if index1 < len(nums1) and index2 < len(nums2):
-                    if nums1[index1] < nums2[index2]:
-                        index1 += 1
-                        continue
-                    else: 
-                        index2 += 1
-                        continue
-                if index1 >= len(nums1):
-                    index2 += 1
-                    continue
-                if index2 >= len(nums2):
-                    index1 += 1
-                    continue
-        if len(answer) == 1:
-            return answer[0]
-        else: 
-            return sum(answer)/2
+        size = len(nums1)+len(nums2)
+        if size%2 == 0:
+            index = [size//2-1, size//2]
+        else:
+            index = [size//2]
+        print(index)
+        result = []
+        i1, i2 = 0, 0
+        for i in range(size):
+            if i1 >= len(nums1):
+                value = nums2[i2]
+                i2 += 1
+            elif i2 >= len(nums2):
+                value = nums1[i1]
+                i1 += 1
+            elif nums1[i1] < nums2[i2]:
+                value = nums1[i1]
+                i1 += 1
+            else:
+                value = nums2[i2]
+                i2 += 1
+            if i in index:
+                result.append(value)
+            if i > max(index):
+                break;
+        return sum(result)
 findMedianSortedArrays([1,3],[2])
